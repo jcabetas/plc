b4e6d9dfee9e0803aadff6a357306d23f03c34e1
Joaquin Cabetas <jcabetas@gmail.com>
2020-04-18 12:29:54 +0200

    He configurado VSCODE

--- bloques.cpp
+++ bloques.cpp
@@ -18,19 +18,89 @@ bloque::bloque()
 }
 
 
+/*
+ * Divide string por espacios
+ * Lo que haya entre [] se considera un solo grupo
+ */
 void divideString(char *buff,uint8_t *numPar, char *par[])
 {
+//  sustituyo CR o LF por nulos
     for (uint8_t i=0;i<strlen(buff);i++)
        if (buff[i]=='\n' || buff[i]=='\r')
           buff[i] = 0;
     *numPar = 0;
-    char* token = strtok(buff, " "); 
-    while (token != NULL) { 
+    while (*buff!=0)
+    {
+        char car = *buff;
+        if (car ==' ')
+        {
+            *(buff++) = 0; // eliminamos espacios en blanco
+            continue;
+        }
+        if (car=='[')
+        {
+            par[*numPar] = buff++;
+            (*numPar)++;
+            while (*buff)
+            {
+                if (*buff == ']') // fin de bloque, ok
+                {
+                    *buff++;
+                    break;
+                }
+                if (*buff==0) // final sin fin de bloque, mal
+                {
+                    printf("Error: '[' sin cierre\n");
+                    *numPar = 0;
+                    return;
+                }
+                buff++;
+            }
+            continue;
+        }
+        if (car==',')
+        {
+            *(buff++) = 0;
+            while (*buff==' ') // eliminamos espacios en blanco
+                *(buff++) = 0;
+            continue;
+        }
+        // estamos en un parametro
+        par[*numPar] = buff++;
+        (*numPar)++;
+        while (*buff!=' ' && *buff!=',' && *buff!='[' && *buff!=0) // recorremos parametros
+        {
+            *(buff++);
+        }
+    }
+}
+
+
+/*
+ * Identifica parametros en bloques [a, b, c]
+ */
+void divideBloque(char *buff,uint8_t *numPar, char *par[])
+{
+    if (buff[0]!='[')
+    {
+        printf("Error, bloque no empieza por '['\n");
+        *numPar = 0;
+        return;
+    }
+    *(buff++);
+//  sustituyo CR o LF por nulos
+    for (uint8_t i=0;i<strlen(buff);i++)
+       if (buff[i]=='\n' || buff[i]=='\r')
+          buff[i] = 0;
+    *numPar = 0;
+    char* token = strtok(buff, ", ]");
+    while (token != NULL) {
         par[*numPar] = token;
         (*numPar)++;
-        token = strtok(NULL, " "); 
-    } 
-}  
+        token = strtok(NULL, ", ]");
+    }
+}
+
 
 int8_t actualizaBloques(bloque **logicas, uint8_t numBlq)
 {
@@ -99,8 +169,8 @@ void simula(bloque **logicas, uint8_t numBlq)
 int main(void)
 {
   estados::init();
-  uint8_t numPar;
-  char *par[15];
+  uint8_t numPar, numParTest;
+  char *par[15], *parTest[15];
   char buffer [120];
   bloque *logicas[20];
   FILE *fich;
@@ -117,6 +187,25 @@ int main(void)
      divideString(buffer,&numPar,par);
      if (numPar==0)
          continue;
+
+     // prueba parametros
+     if (!strcmp("TEST",par[0]))
+     {
+         for (uint8_t i=0;i<numPar;i++)
+         //void divideBloque(char *buff,uint8_t *numPar, char *par[])
+         {
+             if (par[i][0]!='[')
+                 printf("%s\n",par[i]);
+             else
+             {
+                 printf(" bloque {");
+                 divideBloque(par[i],&numParTest, parTest);
+                 for (uint8_t j=0;j<numParTest;j++)
+                     printf("%s ",parTest[j]);
+                 printf("}\n");
+             }
+         }
+     }
      if (numPar>=4 && !strcmp("AND",par[0]))
         logicas[numBlq++] = new add(numPar, par);
      if (numPar>=4 && !strcmp("OR",par[0]))
